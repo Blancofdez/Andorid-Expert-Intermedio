931eea7f58d00894cb2319e5632aa3f2688aec3c
Blancofdez <adanfdezblanco@gmail.com>
2024-06-17 18:16:59 +0200

    permissions management

--- app/src/main/java/com/blancofdez/horosapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/blancofdez/horosapp/ui/palmistry/PalmistryFragment.kt
@@ -4,15 +4,63 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
+import com.blancofdez.horosapp.Manifest
 import com.blancofdez.horosapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.security.Permission
 
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object{
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+
+            //startCamera
+
+        } else {
+
+            Toast.makeText(
+                requireContext(),
+                "Acepata los permisos para utilizar el Palmistry",
+                Toast.LENGTH_LONG
+            ).show()
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (checkCameraPermission()) {
+
+        } else {
+
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+
+        }
+    }
+
+    fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
